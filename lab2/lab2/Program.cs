6c1bb6f81f27b5ffabb8c2c34df10d9238aee087
Luna <lunmars@dunwoody.edu>
2022-04-08 13:05:14 -0500

    adding lab 2 and three work and changing class work to notes

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number One: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 * num2;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number One: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number One: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Boolean condition = num1 > num2;
+            Console.WriteLine("\nBoolean condition is: " + condition);
+
+            if (condition == true)
+            {
+                Console.WriteLine("\nNumber one is larger than number two");
+                Console.WriteLine(num1 + " is larger than " + num2);
+            } 
+            else if (condition == false)
+            {
+                Console.WriteLine("\nNumber one is not greater than number two");
+                Console.WriteLine(num1 + " is larger than " + num2);
+            }
+        }
+    }
+}
